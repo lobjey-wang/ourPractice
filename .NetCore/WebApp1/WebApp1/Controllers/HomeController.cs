35e0afaf62cb64e06706a78a2c3824791016a946
王东林 <pn13572951547@outlook.com>
2021-06-30 09:55:58 +0800

    添加.Net Core练习

---
+++ .NetCore/WebApp1/WebApp1/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp1.Repositories;
+
+namespace WebApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPlayerRepository playerRepository;
+
+        public HomeController(IPlayerRepository playerRep)
+        {
+            playerRepository = playerRep;
+        }
+
+        public string Index()
+        {
+            return "Hello world";
+        }
+
+        public JsonResult Detail(int id)
+        {
+            var player = playerRepository.GetPlayer(id);
+
+            return Json(player);
+        }
+    }
+}
