5c08520a911c3e04d440d7b04bf9c38fa4820793
王东林 <pn13572951547@outlook.com>
2022-03-25 10:30:46 +0800

    commit

---
+++ practice/Practice/MyDelegate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Practice
+{
+    internal class MyDelegate
+    {
+        private event Action<int, int> ActionDelegate;
+        private event Func<int, int, int> FuncDelegate;
+
+        public MyDelegate(Action<int, int> initAction, Func<int, int, int> initFunc)
+        {
+            ActionDelegate += initAction;
+            FuncDelegate += initFunc;
+        }
+
+        public void Excute(int num1, int num2)
+        {
+            if (ActionDelegate != null)
+            {
+                Console.Write($"ActionDelegate: ");
+                ActionDelegate(num1, num2);
+            }
+
+            Console.WriteLine("----------------");
+
+            if (FuncDelegate != null)
+            {
+                Console.WriteLine($"FuncDelegate: {FuncDelegate(num1, num2)}");
+            }
+        }
+    }
+}
---
+++ practice/Practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Practice
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var myDelegate = new MyDelegate(
+                (num1, num2) =>
+                {
+                    Console.Write(num1 + num2);
+                },
+                (num1, num2) =>
+                {
+                    return num1 + num2;
+                }
+            );
+
+            myDelegate.Excute(3, 4);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
